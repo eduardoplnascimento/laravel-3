222cf6b7580fd2a917ea7b504bd1b1e6f0935dc7
Eduardo Nascimento <eduardo.nascimento@mazzafc.tech>
2022-07-14 20:12:05 -0300

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1>Produtos</h1>
+    <a href="/products/create">Criar produto</a>
+
+    <ul>
+        @foreach ($products as $product)
+            <li>
+                {{ $product->name }} - R$ {{ $product->price }}
+                <a href="/products/show/{{ $product->id }}">Ver mais</a>
+            </li>
+        @endforeach
+    </ul>
+</body>
+</html>
